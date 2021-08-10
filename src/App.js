5c83920312d9f7a3271776b7bec95f6ff72e3f45
msenior526 <msenior526@gmail.com>
2021-08-10 17:58:14 -0400

    Import and create signup component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
+import Signup from './components/Signup';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="signup" component={Signup} />
         </div>
     </Router>
     );
