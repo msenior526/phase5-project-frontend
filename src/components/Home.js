7cb473983161cd336114664714ebe7ab60c9f4fe
msenior526 <msenior526@gmail.com>
2021-08-10 15:48:19 -0400

    Create state for home component and use lifecycle method for fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
 import React from "react";
 
-
 class Home extends React.Component{
-    render(){return (
-        <div>
-            <h1>Home!</h1>
-        </div>
-    )}
+    state = {
+        articles: []
+    }
+
+    componentDidMount() {
+        return fetch('http://localhost:3000/articles')
+        .then(resp => resp.json())
+        .then(articles => {
+            this.setState({
+                articles: articles
+            })
+            console.log(this.state);
+        })
+    }
+    
+    render() {
+        return (
+            <div><h1>Home!</h1></div>
+        )
+    }
 }
 
 export default Home;
